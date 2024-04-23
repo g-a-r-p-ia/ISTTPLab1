fc6f1a48f6a68f7389329ebb2dd157d052a8bbfc
g-a-r-p-ia <88767452+g-a-r-p-ia@users.noreply.github.com>
2024-04-23 07:04:23 +0300

    1.4 and 1.5 part of lab1

---
+++ ISSTTP/Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using ISSTTP.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ISSTTP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private record CountByCategoryResponseItem(string Category, int Count);
+        private readonly DbcarShopContext dbCarShopContext;
+
+        public ChartController(DbcarShopContext dbCarShopContext)
+        {
+            this.dbCarShopContext = dbCarShopContext;
+        }
+        [HttpGet("countByCategory")]
+        public async Task<JsonResult> GetCountByCategoryAsync(CancellationToken cancellationToken)
+        {
+            var responseItems = await dbCarShopContext
+            .Details
+            .GroupBy(purchase => purchase.CategoryId)
+            .Select(group => new CountByCategoryResponseItem(group.Key.ToString(), group.Count()))
+            .ToListAsync(cancellationToken);
+
+            return new JsonResult(responseItems);
+        }
+
+    }
+}
